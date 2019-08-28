08d4de5ea2b3df93a27b51038e16d3179980a0e7
Aaron Holm <aholm@seia.org>
2019-08-28 16:31:29 -0400

    added national rows

--- grouped_bar_chart.R
+++ grouped_bar_chart.R
@@ -17,29 +17,28 @@ db_host="data.seia.org"
 db_port=5432
 db_name="seia"
 
-#group_data = read_excel('C:/tmp/ITC/Clean Files/WoodMac_IO_run_6_calibrated_v2_chart_data.xlsx')
 # Get Data
 pg = dbDriver("PostgreSQL")
 con = dbConnect(pg, user=db_user, password=db_pass, host=db_host, port=db_port, dbname=db_name)
 group_data = dbGetQuery(con, "select * from products.itc_2019")
-
+group_data
 # Transform data
 us_data = group_data %>% group_by(year, scenario) %>%
                         summarise(jobs=sum(jobs, na.rm=T))
-us_data$state_full = 'USA'
 us_data
+us_data$state_full = 'USA'
 state_data = group_data %>% group_by(year, state_full, scenario) %>%
                             summarise(jobs=sum(jobs, na.rm=T))
 
 data = rbind(state_data, us_data)
-
+data
 # Make the Chart
 #data
 #us = data %>% filter(state_full=='USA')
 
-states = unique(group_data$state_full)
+states = unique(data$state_full)
 for (state in states){
-  state_data <- group_data %>% filter(state_full==state) %>% 
+  state_data <- data %>% filter(state_full==state) %>% 
     group_by(year, scenario) %>%
     summarise(jobs=sum(jobs))
   
@@ -49,8 +48,7 @@ for (state in states){
     seia_style()+
     scale_y_continuous(expand=expand_scale(mult=c(0,0.02)), label=comma) 
   #guides(fill=guide_legend(reverse=TRUE))
-  
-  ggsave(fileloc, width=18, height=7, dpi=300)
+  ggsave(paste("C:/tmp/ITC/charts/", state, '.png', sep=""), width=18, height=7, dpi=300)
   
 }
 
